5fc837aae6896bc6ccb6085a5f4e0bcaaba8e748
Sid Sun <sid@sidsun.com>
2026-06-14 08:41:23 +0530

    Re-implement topic test before subscribe
    Despite the regex, there are some edgecases where a private or other wise invalid topic can sneak into the subscription list and cause the bot's subscriber to fail. Testing on the actual topic to be subscribed to should address all of these edgecases.
    
    Signed-off-by: Sid Sun <sid@sidsun.com>

--- pkg/bot/handlers/subscribe/subscribe.go
+++ pkg/bot/handlers/subscribe/subscribe.go
@@ -1,10 +1,17 @@
 package subscribe
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"log/slog"
+	"net/http"
+	"strings"
+	"time"
 
+	"github.com/gorilla/websocket"
+	"github.com/leonklingele/passphrase"
+	"github.com/sid-sun/ntfy.tg/cmd/config"
 	"github.com/sid-sun/ntfy.tg/pkg/metrics"
 	subscriptionmanager "github.com/sid-sun/ntfy.tg/pkg/subscription_manager"
 	tele "gopkg.in/telebot.v4"
@@ -28,6 +35,12 @@ func Hnadler(c tele.Context) error {
 		return errors.New("invalid topic provided")
 	}
 
+	if err := testTopic(topic); err != nil {
+		slog.Error(fmt.Sprintf("[Subscribe] [testTopic] %s", err.Error()))
+		c.Reply(fmt.Sprintf("topic test failed: %s", err.Error()))
+		return err
+	}
+
 	subscriptionmanager.SubscribeChatToTopic(topic, chatID)
 
 	if err := c.Reply("topic test successful, you are now subscribed to topic"); err != nil {
@@ -37,4 +50,55 @@ func Hnadler(c tele.Context) error {
 
 	slog.Info("[Subscribe] [Success]")
 	return nil
+}
+
+func testTopic(topic string) error {
+	passphrase.Separator = "-"
+	randomMessage, err := passphrase.Generate(4)
+	if err != nil {
+		return fmt.Errorf("generating test message: %w", err)
+	}
+
+	domain := config.GetConfig().GetNtfyDomain()
+	wsURL := fmt.Sprintf("wss://%s/%s/ws?since=%d", domain, topic, time.Now().Unix())
+
+	conn, _, err := websocket.DefaultDialer.Dial(wsURL, nil)
+	if err != nil {
+		return fmt.Errorf("connecting to topic: %w", err)
+	}
+	defer conn.Close()
+
+	result := make(chan error, 1)
+	go func() {
+		conn.SetReadDeadline(time.Now().Add(15 * time.Second))
+		for {
+			_, msg, err := conn.ReadMessage()
+			if err != nil {
+				result <- fmt.Errorf("waiting for test message: %w", err)
+				return
+			}
+			var m publishedMessage
+			if err := json.Unmarshal(msg, &m); err != nil {
+				continue
+			}
+			if m.Event == "message" && m.Message == randomMessage {
+				result <- nil
+				return
+			}
+		}
+	}()
+
+	postURL := fmt.Sprintf("https://%s/%s", domain, topic)
+	req, _ := http.NewRequest(http.MethodPost, postURL, strings.NewReader(randomMessage))
+	req.Header.Set("Content-Type", "text/plain")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("publishing test message: %w", err)
+	}
+	resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("publishing test message: HTTP %d", resp.StatusCode)
+	}
+
+	return <-result
 }
